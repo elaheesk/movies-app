38d049ecd1452fc9f0100c9c9aa3c0d65ba01d88
Elahe Eskandari <74405627+elaheesk@users.noreply.github.com>
2022-03-03 09:56:18 +0100

    fixed the cardlayout component

--- src/components/CardLayout.tsx
+++ src/components/CardLayout.tsx
@@ -61,96 +61,104 @@ const CardLayout: React.FC<CardLayoutProps> = ({
 
   return (
     <Grid item marginRight={4} marginBottom={4}>
-      <Card sx={{ maxWidth: 345, height: 400 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={`${apiImg}${showObject?.backdrop_path}`}
-          alt="pic"
-          onClick={handleOpen}
-        />
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Overview
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              {showObject?.overview}
-            </Typography>
-          </Box>
-        </Modal>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {showObject?.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {showObject?.release_date}
-          </Typography>
-
-          <Grid container justifyContent="space-between" marginY={2}>
-            <Grid item>
-              <Typography fontWeight={"light"} variant="caption">
-                <Link to={`/${linkName}/${showObject?.id}`}>Read more</Link>
+      <Card sx={{ width: 330, height: 400 }}>
+        <Grid item>
+          {" "}
+          <CardMedia
+            component="img"
+            height="140"
+            image={`${apiImg}${showObject?.backdrop_path}`}
+            alt="pic"
+            onClick={handleOpen}
+          />
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Overview
               </Typography>
-            </Grid>
-            <Grid item>
-              {showObject?.liked ? (
-                <FavoriteIcon
-                  style={{ fill: "red" }}
-                  onClick={() => handleLike(showObject)}
-                />
-              ) : (
-                <FavoriteIcon
-                  style={{ fill: "gray" }}
-                  onClick={() => handleLike(showObject)}
-                />
-              )}
-            </Grid>
-          </Grid>
-        </CardContent>
-        <CardContent>
-          <Grid container direction="column">
-            <Grid item>
-              <Button
-                disableElevation
-                size="small"
-                onClick={() => handleEditComment(showObject)}
-              >
-                Leave a comment
-              </Button>
-            </Grid>
-            <Grid item>
-              {!reviewField && (
-                <Typography>{showObject?.writeComment}</Typography>
-              )}
-            </Grid>
-            {showObject?.editMode && (
-              <React.Fragment>
-                <Grid item>
-                  <input
-                    type="text"
-                    value={reviewField}
-                    onChange={(event) => {
-                      setReviewField(event.target.value);
-                    }}
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {showObject?.overview}
+              </Typography>
+            </Box>
+          </Modal>
+        </Grid>
+
+        <Grid item>
+          {" "}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {showObject?.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {showObject?.release_date}
+            </Typography>
+
+            <Grid container justifyContent="space-between" marginY={2}>
+              <Grid item>
+                <Typography fontWeight={"light"} variant="caption">
+                  <Link to={`/${linkName}/${showObject?.id}`}>Read more</Link>
+                </Typography>
+              </Grid>
+              <Grid item>
+                {showObject?.liked ? (
+                  <FavoriteIcon
+                    style={{ fill: "red" }}
+                    onClick={() => handleLike(showObject)}
                   />
-                </Grid>
-                <Grid item>
-                  <Button onClick={() => handleSaveComment(showObject)}>
-                    save
-                  </Button>
-                </Grid>
-              </React.Fragment>
-            )}
-            <Grid item></Grid>
-            <Grid item></Grid>
-          </Grid>
-        </CardContent>
+                ) : (
+                  <FavoriteIcon
+                    style={{ fill: "gray" }}
+                    onClick={() => handleLike(showObject)}
+                  />
+                )}
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Grid>
+        <Grid item>
+          {" "}
+          <CardContent>
+            <Grid container direction="column">
+              <Grid item>
+                <Button
+                  disableElevation
+                  size="small"
+                  onClick={() => handleEditComment(showObject)}
+                >
+                  Leave a comment
+                </Button>
+              </Grid>
+              <Grid item>
+                {!reviewField && (
+                  <Typography>{showObject?.writeComment}</Typography>
+                )}
+              </Grid>
+              {showObject?.editMode && (
+                <React.Fragment>
+                  <Grid item>
+                    <input
+                      type="text"
+                      value={reviewField}
+                      onChange={(event) => {
+                        setReviewField(event.target.value);
+                      }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Button onClick={() => handleSaveComment(showObject)}>
+                      save
+                    </Button>
+                  </Grid>
+                </React.Fragment>
+              )}
+            </Grid>
+          </CardContent>
+        </Grid>
       </Card>
     </Grid>
   );
